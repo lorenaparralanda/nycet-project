89d3c4fb69c5c4aa8f1f85813607907609454c7c
Carolina Gonzalez <cgonzalez@ckmadvisors.com>
2018-07-19 11:30:58 -0400

    url fix

--- competitiveness/src/components/ParentHeader.jsx
+++ competitiveness/src/components/ParentHeader.jsx
@@ -4,25 +4,26 @@ import { withRouter } from 'react-router-dom';
 import RegionPicker from './RegionPicker.jsx';
 const React = require('react');
 
-const ParentHeaderContainer = withRouter(({districtType, changeDistrict, history}) => (
+const ParentHeaderContainer = withRouter(({districtType, region, changeDistrict, history}) => (
     <h1><RegionPicker /> Competitiveness - <Dropdown options={
         [{text: 'Congressional District', value: 'CD'},
          {text: 'State Senate District', value: 'SD'},
          {text: 'State Assembly District', value: 'AD'},
          ]}
          defaultValue={districtType}
-         onChange={(e, d) => changeDistrict(e, d, history)}
+         onChange={(e, d) => changeDistrict(e, d, history, region)}
          className='district-select'
        />
     </h1>
 ))
 
 const mapStateToProps = (state) => ({
-  districtType: state.districtType })
+  districtType: state.districtType,
+  region: state.selectedRegion })
 
 const mapDispatchToProps = (dispatch) => ({
-  changeDistrict: (e, d, h) => (
-      h.push(`/${d.value}`))
+  changeDistrict: (e, d, h, r) => (
+      h.push(`/${d.value}?region=${r}`))
   })
 
 export default connect(mapStateToProps, mapDispatchToProps)(ParentHeaderContainer)
